790dfec95819e2380b760e992f6582aa5ad6c6fe
Ceyhun Yusif <jeyhun312@gmail.com>
2026-02-25 16:57:04 +0400

    SummerSchool,Universty,LanguageCourse (updated) were written

---
+++ Migrations/20260225125636_mig-9.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApexWebAPI.Migrations
+{
+    public partial class mig9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "UniversityTranslations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Universities",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "SummerSchoolTranslations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "SummerSchools",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "LanguageCourseTranslations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "LanguageCourses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "UniversityTranslations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Universities");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "SummerSchoolTranslations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "SummerSchools");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "LanguageCourseTranslations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "LanguageCourses");
+        }
+    }
+}
