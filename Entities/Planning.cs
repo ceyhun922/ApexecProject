7ccf03a1c55c0a807a6f572dd05c8217359b7674
Ceyhun Yusif <jeyhun312@gmail.com>
2026-02-26 13:45:14 +0400

    Planing entity (updated)

--- Entities/Planning.cs
+++ Entities/Planning.cs
@@ -3,13 +3,9 @@ namespace ApexWebAPI.Entities
     public class Planning
     {
         public int Id { get; set; }
-        public string? Option1 { get; set; }
-        public string? Option2 { get; set; }
-        public string? Option3 { get; set; }
-        public string? Option4 { get; set; }
-        public bool Checkbox1 { get; set; }
-        public bool Checkbox2 { get; set; }
-        public bool Checkbox3 { get; set; }
+        public string? Badge { get; set; }
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
         public bool Status { get; set; } = true;
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 
